245c259f624228cbdd308ff6dd4bf88fcefda862
zzsound <zzsound1@gmail.com>
2025-04-07 11:51:28 +0200

    commit 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://zzsound.github.io',
+
+     base: '/',
+
+});
